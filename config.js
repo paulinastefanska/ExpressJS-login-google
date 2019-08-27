69a0e502189ad5f0596e23a0f5418d4f11f14629
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-08-27 10:34:06 +0200

    Add configurations

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports= {
+	'GOOGLE_CLIENT_ID': '643625167573-72rnmfrfhd0vlcr3s9r02kodce14ge2k.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'nvQKYXydLDXyhgeskTg82zzt',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
